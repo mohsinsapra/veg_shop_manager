5d5a91b296676ae4a57c6aa849ca57239c7cebc9
Muhammad Mohsin <mohsin.sapra@gmail.com>
2026-07-01 22:31:57 +0200

    deploy 2026-07-01T22:31

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1999090584" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2166350337" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
