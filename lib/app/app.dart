e3b85925670a4be2a433043c69c6d814903565ef
Muhammad Mohsin <mohsin.sapra@gmail.com>
2026-07-01 20:47:15 +0200

    feature/cloud-inventory-printing: wire AppLocalizations + locale into MaterialApp.router

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:veg_shop_manager/l10n/app_localizations.dart';
 import 'router/app_router.dart';
 import 'theme/app_theme.dart';
 import '../core/constants/app_constants.dart';
+import '../presentation/providers/locale_provider.dart';
 
 class VegShopApp extends ConsumerWidget {
   const VegShopApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final locale = ref.watch(localeProvider);
+
     return MaterialApp.router(
       title: AppConstants.appName,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      locale: locale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       routerDelegate: AppRouter.routerDelegate,
       routeInformationParser: AppRouter.routeInformationParser,
       debugShowCheckedModeBanner: false,
